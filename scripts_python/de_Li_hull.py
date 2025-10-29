65463b48d8e844c09bff3843fee2d0992105e214
caijunfei-max <caijf8223@163.com>
2025-10-29 16:30:34 +0800

    de_li update

--- scripts_python/de_Li_hull.py
+++ scripts_python/de_Li_hull.py
@@ -5,14 +5,15 @@
 # @Software: PyCharm
 
 from pymatgen.core import Structure
-from itertools import combinations
-import random
-import copy
-import os
+import numpy as np
+
 
 """
 本脚本用于convex hull计算工作流，直接挖取运行一次会随机挖取一个锂离子、生成对应的结构。
 """
+sites = [i for i in range(16)]
+structure = Structure.from_file("CONTCAR")
+de_structure = structure.copy()
+de_structure.remove_sites(np.random.choice(sites, size=12, replace=False))
 
-structure = Structure.from_file("POSCAR")
-
+de_structure.to(filename="de_CONTCAR", fmt="poscar")
